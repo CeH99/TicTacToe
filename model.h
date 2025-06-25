603e2943bf8a025e1019f89de6e4f8168c5fb4a4
Semen <senya183381@gmail.com>
2025-06-25 13:29:13 +0300

    first

---
+++ model.h
@@ -0,0 +1,32 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#define ROW 3
+#define COL 3
+
+#include <QObject>
+
+class Model : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QVariant board READ board NOTIFY boardChanged)
+
+public:
+    explicit Model(QObject* parent = nullptr);
+    ~Model();
+
+    void setCell(int row, int col, int value);
+    int getCell(int row, int col) const;
+
+    int check_Win();
+
+    QVariant board() const;
+
+signals:
+    void boardChanged();
+
+private:
+    int arr[ROW][COL]; // можно статично, нам не нужен dynamic
+};
+
+#endif // MODEL_H
