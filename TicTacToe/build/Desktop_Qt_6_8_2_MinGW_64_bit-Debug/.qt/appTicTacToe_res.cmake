f069a51726e9e099c31858870f2675822aaa250a
CeH99 <senya183381@gmail.com>
2025-07-01 23:07:37 +0000

    deleted files

--- TicTacToe/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/appTicTacToe_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appTicTacToe")
-set(working_dir "S:/HomeWorks-Hillel/zxc/TicTacToe")
-set(src_and_dest_list
-
-)
-set(timestamp_file "S:/HomeWorks-Hillel/zxc/TicTacToe/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/appTicTacToe_res.txt")
--- TicTacToe/build/Desktop_Qt_6_9_1-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appTicTacToe_FILE /home/user/Qt_Projects/TicTacToe/TicTacToe/build/Desktop_Qt_6_9_1-Debug/appTicTacToe)
-set(__QT_DEPLOY_TARGET_appTicTacToe_TYPE EXECUTABLE)
